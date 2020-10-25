117f8385ad7fa2e55e2cd6ffcf5abc47783644e6
andreahu <andreahu825@gmail.com>
2020-10-25 16:37:34 -0700

    na

--- SmartCity/src/cscie97/smartcity/controller/CO2Cmd.java
+++ SmartCity/src/cscie97/smartcity/controller/CO2Cmd.java
@@ -2,12 +2,16 @@ package cscie97.smartcity.controller;
 
 import cscie97.smartcity.model.*;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class CO2Cmd implements Command {
 
 
     private Event event;
     private City city;
     private String co2level;
+    private ArrayList<Vehicle> cars;
 
     public CO2Cmd(Event event) {
         this.event = event;
@@ -25,10 +29,43 @@ public class CO2Cmd implements Command {
     }
 
     public void co2high() {
+        //TODO: If reported by more than 3 devices within a city,
 
+
+        //Disable all cars in the city
+        this.cars = getAllCars();
+        disableAllCars(cars);
     }
 
     public void co2ok() {
+        //TODO: If reported by more than 3 devices within a city,
 
+
+        //Enable all cars in the city
+        this.cars = getAllCars();
+        enableAllCars(cars);
+    }
+
+    public ArrayList<Vehicle> getAllCars() {
+        Map<String, Device> deviceMap = city.getDeviceMap();
+        ArrayList<Vehicle> cars = new ArrayList<>();
+        for (Device d : deviceMap.values()) {
+            if (d instanceof Vehicle) {
+                cars.add((Vehicle) d);
+            }
+        }
+        return cars;
+    }
+
+    public void disableAllCars(ArrayList<Vehicle> cars) {
+        for (Vehicle v : cars) {
+            v.setEnabled("false");
+        }
+    }
+
+    public void enableAllCars(ArrayList<Vehicle> cars) {
+        for (Vehicle v : cars) {
+            v.setEnabled("true");
+        }
     }
 }
