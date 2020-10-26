f49e27d89bc0ca1f6ad7f0cbfa05e9cbf817debf
andreahu <andreahu825@gmail.com>
2020-10-26 16:22:26 -0700

    na

--- SmartCity/src/cscie97/smartcity/controller/EmergencyCmd.java
+++ SmartCity/src/cscie97/smartcity/controller/EmergencyCmd.java
@@ -5,6 +5,17 @@ import cscie97.smartcity.model.*;
 import java.util.ArrayList;
 import java.util.Map;
 
+/**
+ * This cmd calls different emergency execution based on the emergency type
+ * For traffic_accident:
+ * 1. The reporting device announces: "Stay calm, help is on its way"
+ * 2. Nearest 2 Robots: "address <emergency_type> at lat <lat> long <long>"
+ * For all other emergency type:
+ * 1. The city announce: "There is a
+ * <emergency_type> in <city>, please find shelter immediately"
+ * 2. 1⁄2 Robots: "address <emergency_type> at lat <lat> long <long>"
+ * 3. remaining robots: "Help people find shelter"
+ */
 public class EmergencyCmd implements Command {
 
     private Event event;
@@ -14,6 +25,11 @@ public class EmergencyCmd implements Command {
     private float lat;
     private float lon;
 
+    /**
+     * Call different emergency execution based on the emergency type
+     *
+     * @param event
+     */
     public EmergencyCmd(Event event) {
         this.event = event;
         this.city = event.getCity();
@@ -34,7 +50,12 @@ public class EmergencyCmd implements Command {
         }
     }
 
-
+    /**
+     * 1. The city announce: "There is a
+     * <emergency_type> in <city>, please find shelter immediately"
+     * 2. 1⁄2 Robots: "address <emergency_type> at lat <lat> long <long>"
+     * 3. remaining robots: "Help people find shelter"
+     */
     public void emergency1_execute() {
         city.setAnnouncement("There is a " + emergency_type + " in " + city.getCityId() + ", please find shelter immediately");
 
@@ -48,6 +69,10 @@ public class EmergencyCmd implements Command {
         }
     }
 
+    /**
+     * 1. The reporting device announces: "Stay calm, help is on its way"
+     * 2. Nearest 2 Robots: "address <emergency_type> at lat <lat> long <long>"
+     */
     public void emergency2_execute() {
         device.setAnnouncement("Stay calm, help is on its way");
         Robot[] nearest2Robots = findNearest2Robots();
@@ -55,6 +80,9 @@ public class EmergencyCmd implements Command {
         nearest2Robots[1].setActivity("address " + emergency_type + " at lat " + lat + "long" + lon);
     }
 
+    /**
+     * @return a list of all robots in the city
+     */
     public ArrayList<Robot> getAllRobots() {
         Map<String, Device> deviceMap = city.getDeviceMap();
         ArrayList<Robot> robots = new ArrayList<>();
@@ -66,6 +94,13 @@ public class EmergencyCmd implements Command {
         return robots;
     }
 
+    /**
+     * Find the distance between a device and an event
+     *
+     * @param e the event
+     * @param d the device
+     * @return the distance between the two paramater objects
+     */
     public float distance(Event e, Device d) {
         float lat1 = e.getLat();
         float lon1 = e.getLon();
@@ -75,6 +110,9 @@ public class EmergencyCmd implements Command {
         return distance;
     }
 
+    /**
+     * @return 2 robots in the city that's the closest to the event
+     */
     public Robot[] findNearest2Robots() {
         ArrayList<Robot> robots = getAllRobots();
         Robot[] nearest2Robots = new Robot[2];
